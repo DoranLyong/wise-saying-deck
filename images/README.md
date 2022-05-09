facc8fa6bc3246661279f0932f59f71a10a8a140
DoranLyong <cheeryun@gmail.com>
2022-05-09 10:14:17 +0900

    canvas

---
+++ images/README.md
@@ -0,0 +1,4 @@
+# Reference 
+
+1. https://www.makeuseof.com/tag/how-to-change-the-background-of-a-photo-in-photoshop/
+2. https://www.pexels.com/photo/silhouette-of-a-person-on-a-swing-3293148/
--- utils/tools.py
+++ utils/tools.py
@@ -7,5 +7,6 @@ def random_bg(dir_path:str, ext="png") -> str:
     """ return an image path in random 
     """
     img_list = glob.glob(osp.join(dir_path, f"*.{ext}"))
+    random.shuffle(img_list)
 
     return random.choice(img_list)
